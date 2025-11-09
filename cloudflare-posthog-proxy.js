abb8dde29b42c9241b725fdb6ef11ef832cae5f1
Eeshan Srivastava <77134595+eeshansrivastava89@users.noreply.github.com>
2025-11-09 11:01:14 -0800

    feat: add PostHog reverse proxy via Cloudflare Worker to bypass ad blockers

---
+++ cloudflare-posthog-proxy.js
@@ -0,0 +1,40 @@
+// Cloudflare Worker for PostHog Reverse Proxy
+// Official implementation from https://posthog.com/docs/advanced/proxy/cloudflare
+
+const API_HOST = "us.i.posthog.com"
+const ASSET_HOST = "us-assets.i.posthog.com"
+
+async function handleRequest(request, ctx) {
+  const url = new URL(request.url)
+  const pathname = url.pathname
+  const search = url.search
+  const pathWithParams = pathname + search
+
+  if (pathname.startsWith("/static/")) {
+    return retrieveStatic(request, pathWithParams, ctx)
+  } else {
+    return forwardRequest(request, pathWithParams)
+  }
+}
+
+async function retrieveStatic(request, pathname, ctx) {
+  let response = await caches.default.match(request)
+  if (!response) {
+    response = await fetch(`https://${ASSET_HOST}${pathname}`)
+    ctx.waitUntil(caches.default.put(request, response.clone()))
+  }
+  return response
+}
+
+async function forwardRequest(request, pathWithSearch) {
+  const originRequest = new Request(request)
+  originRequest.headers.delete("cookie")
+  return await fetch(`https://${API_HOST}${pathWithSearch}`, originRequest)
+}
+
+export default {
+  async fetch(request, env, ctx) {
+    return handleRequest(request, ctx);
+  }
+};
+
